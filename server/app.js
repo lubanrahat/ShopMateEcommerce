36e4d190a32ab738d87519c3417c8210984cd720
Luban Rahat <lubanrahat.dev@gmail.com>
2025-10-23 14:58:32 +0600

    init server

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+
+dotenv.config({ path: "./.env" });
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DETETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    tempFileDir: "./uploads",
+    useTempFiles: true,
+  })
+);
+
+export default app;
